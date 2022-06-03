b42b435038a07bc4bb196d67030c62f2bc220f60
Aviatorscode2 <victoryakubu5@gmail.com>
2022-06-03 04:46:07 +0100

    Proper routing and home page for each tutorial

---
+++ src/components/Home.jsx
@@ -0,0 +1,56 @@
+import { Link } from 'react-router-dom';
+
+// home page directing you to the various hooks tutorials.
+function Home() {
+  return (
+    <>
+      <h1 className="mb-3">React Sandbox</h1>
+      <ul class="list-group">
+        <Link
+          className="list-group-item list-group-item-action"
+          to="useref-example-1"
+        >
+          useRef Example 1
+        </Link>
+        <Link
+          className="list-group-item list-group-item-action"
+          to="useref-example-2"
+        >
+          useRef Example 2
+        </Link>
+        <Link
+          className="list-group-item list-group-item-action"
+          to="useref-example-3"
+        >
+          useRef Example 3
+        </Link>
+        <Link
+          className="list-group-item list-group-item-action"
+          to="usememo-example"
+        >
+          useMemo Example
+        </Link>
+        <Link
+          className="list-group-item list-group-item-action"
+          to="usecallback-example"
+        >
+          useCallback Example
+        </Link>
+        <Link
+          className="list-group-item list-group-item-action"
+          to="custom-hook-example-1"
+        >
+          Custom Hook - useFetch()
+        </Link>
+        <Link
+          className="list-group-item list-group-item-action"
+          to="custom-hook-example-2"
+        >
+          Custom Hook - useLocalStorage()
+        </Link>
+      </ul>
+    </>
+  );
+}
+
+export default Home;
